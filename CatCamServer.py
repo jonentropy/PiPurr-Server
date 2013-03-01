b5c524890e579019416951f97095f61d2a5cfb76
Tristan Linnell <talinnell@gmail.com>
2013-03-01 18:26:12 +0000

    Add LEDBorg and sound playback.

--- CatCamServer.py
+++ CatCamServer.py
@@ -7,14 +7,21 @@
 #	for any filename. Intended to work with a simple
 #	Android app that fetches the image.
 #
+#   Also flashes colours on an LEDBorg from www.piborg.org
+#   and can play back a sound on the RPi's connected 
+#   speakers.
+#
 #	Tris Linnell
 #		http://canthack.org
+
+print "Initialising..."
 
 from cv2 import *
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
 from datetime import datetime
 import time
+import pygame
 
 #Constants
 PORT_NUMBER = 8081
@@ -41,54 +48,62 @@ class myHandler(BaseHTTPRequestHandler):
 	
 	#Overload logging function with our own
 	def log_message(self, format, *args):
-		Log(self.address_string() + ' ' + format%args);
+		Log(self.address_string() + ' (' + str(self.client_address[0]) + ') ' + format%args);
 		
 	#Handler for the GET requests, which is all we are handling...
 	def do_GET(self):
 		#Ignore favicon requests to keep logs clean
 		if self.path == '/favicon.ico':
-			return
+			pass
 
-		#Only continue if the server is asking for /cats.jpeg. Send
-		#403 Forbidden HTTP response otherwise
-		if self.path != '/cats.jpeg':
-			self.send_error(403, 'Forbidden')
-			flashColour(RED)
-			return
-			
-		try:	
-			#Capture the image
-			camera.release()
-			camera.open(0)
+		#Only continue if the server is asking for a known URI. Send
+		#403 Forbidden HTTP response otherwise.
 		
-			#Set image dimensions. v4l and your webcam must support this
-			camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320);
-			camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240);
+		elif self.path == '/sound':
+			#play sound
+			pygame.mixer.music.load("sound.wav")
+			pygame.mixer.music.play()
+			self.send_response(200, "Sound played OK");
+			flashColour(BLUE);
 		
-			status, image = camera.read()
+		elif self.path == '/cats.jpeg':				
+			try:	
+				#Capture the image
+				camera.release()
+				camera.open(0)
 			
-			text = datetime.now().strftime("%H:%M:%S %a %d")
-			textcolour = (100, 100, 120)
- 		
-			putText(image, text, (2,20), FONT_HERSHEY_COMPLEX_SMALL, 1.0, textcolour)
-
-			if status:
-				self.send_response(200)
-				self.send_header('Content-type', 'image/jpg')
-				self.end_headers()
-				st, buffger = imencode('.jpg', image)
-				self.wfile.write(buffger.tostring())
-				flashColour(GREEN)
-				return
-			else:
-				#Something went wrong while creating the image,
-				#Send 500 Internal Server Error
-				self.send_error(500, 'Image capture failed')
+				#Set image dimensions. v4l and your webcam must support this
+				camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320);
+				camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240);
+			
+				status, image = camera.read()
+				
+				text = datetime.now().strftime("%H:%M:%S %a %d")
+				textcolour = (100, 100, 120)
+	 		
+				putText(image, text, (2,20), FONT_HERSHEY_COMPLEX_SMALL, 1.0, textcolour)
+	
+				if status:
+					self.send_response(200)
+					self.send_header('Content-type', 'image/jpg')
+					self.end_headers()
+					st, buffger = imencode('.jpg', image)
+					self.wfile.write(buffger.tostring())
+					flashColour(GREEN)
+				else:
+					#Something went wrong while creating the image,
+					#Send 500 Internal Server Error
+					self.send_error(500, 'Image capture failed')
+					flashColour(RED)
+	
+			except IOError:
+				self.send_error(404,'File Not Found: %s' % self.path)
 				flashColour(RED)
-
-		except IOError:
-			self.send_error(404,'File Not Found: %s' % self.path)
-			flashColour(RED)
+				
+		else:
+			#Unknown URI
+			self.send_error(403, 'Forbidden')
+			flashColour(RED)	
 
 logging = False;
 
@@ -111,6 +126,9 @@ try:
 	#Open the cat cam
 	camera = VideoCapture(0)
 
+	#For sound playback
+	pygame.init()
+	
 	#Create the web server to serve the cat pics
 	server = HTTPServer(('', PORT_NUMBER), myHandler)
 	Log('Cat pic server started on port ' + str(PORT_NUMBER))
