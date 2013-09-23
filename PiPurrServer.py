a83955fae86f744a46c14ea2ed119e0b96077400
Tristan Linnell <talinnell@gmail.com>
2013-09-23 18:18:30 +0000

    Add feeder function.

--- PiPurrServer.py
+++ PiPurrServer.py
@@ -5,6 +5,8 @@
 #
 #   Allows remote interaction with your cats.
 #
+#   Contains code from piborg.org
+#
 #	Tris Linnell
 #		http://canthack.org
 
@@ -16,10 +18,73 @@ import os
 from datetime import datetime
 import time
 import pygame
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
 
 #Constants
 PORT_NUMBER = 8081
 
+# Set which GPIO pins the drive outputs are connected to
+# for feeder control (stepper motor)
+DRIVE_1 = 4  # Black
+DRIVE_2 = 18 # Green
+DRIVE_3 = 8  # Red
+DRIVE_4 = 7  # Blue
+
+# Tell the system how to drive the stepper
+sequence = [DRIVE_1, DRIVE_3, DRIVE_2, DRIVE_4] # Order for stepping (see data sheet for the stepper motor)
+stepDelay = 0.006                               # Delay between steps
+
+# Set all of the drive pins as output pins
+GPIO.setup(DRIVE_1, GPIO.OUT)
+GPIO.setup(DRIVE_2, GPIO.OUT)
+GPIO.setup(DRIVE_3, GPIO.OUT)
+GPIO.setup(DRIVE_4, GPIO.OUT)
+
+# Function to set all drives off
+def MotorOff():
+    global step
+    GPIO.output(DRIVE_1, GPIO.LOW)
+    GPIO.output(DRIVE_2, GPIO.LOW)
+    GPIO.output(DRIVE_3, GPIO.LOW)
+    GPIO.output(DRIVE_4, GPIO.LOW)
+    step = -1
+    
+# Function to perform a sequence of steps as fast as allowed
+def MoveStep(count):
+    global step
+
+    # Choose direction based on sign (+/-)
+    if count < 0:
+        dir = -1
+        count *= -1
+    else:
+        dir = 1
+
+    # Loop through the steps
+    while count > 0:
+        # Set a starting position if this is the first move
+        if step == -1:
+            GPIO.output(DRIVE_4, GPIO.HIGH)
+            step = 0
+        else:
+            step += dir
+
+        # Wrap step when we reach the end of the sequence
+        if step < 0:
+            step = len(sequence) - 1
+        elif step >= len(sequence):
+            step = 0
+
+        # For this step turn one of the drives off and another on
+        if step < len(sequence):
+            GPIO.output(sequence[step-2], GPIO.LOW)
+            GPIO.output(sequence[step], GPIO.HIGH)
+        time.sleep(stepDelay)
+        count -= 1
+
+# LedBorg colours
 RED = (1,0,0)
 GREEN = (0,1,0)
 BLUE = (0,0,1)
@@ -40,7 +105,7 @@ def flashColour(colour):
 #HTTP Server class
 class myHandler(BaseHTTPRequestHandler):	
 	
-	#Overload logging function with our own
+	#Override logging function with our own
 	def log_message(self, format, *args):
 		Log(self.address_string() + " (" + str(self.client_address[0]) + ") " + format%args);
 		
@@ -52,6 +117,23 @@ class myHandler(BaseHTTPRequestHandler):
 
 		#Only continue if the server is asking for a known URI. Send
 		#403 Forbidden HTTP response otherwise.
+		
+		elif self.path == "/feed":
+			#feed cats
+			MotorOff()
+			MoveStep(200); #will be specific to your motor
+			MotorOff()
+			self.send_response(200, "Fed OK");
+			
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+			
+			self.wfile.write("<html><head><title>" + os.path.basename(__file__) + "</title></head>")
+			self.wfile.write("<body><p>Fed OK!</p>")
+			self.wfile.write("</body></html>")
+			self.wfile.close()
+			
+			flashColour(BLUE);
 		
 		elif self.path == "/sound":
 			#play sound
@@ -132,6 +214,9 @@ except Exception, e:
 	print "Logging disabled, %s" %e
 		
 try:		
+	# shut feeder motor off
+	MotorOff()
+    
 	#Open the cat cam
 	camera = VideoCapture(0)
 
@@ -151,3 +236,5 @@ except KeyboardInterrupt:
 	Log("Shutting down...")
 	LogFile.close()
 	server.socket.close()
+	MotorOff()
+	GPIO.cleanup()
